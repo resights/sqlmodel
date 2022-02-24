723a22a4b86e56551d6ca901ce000ed50dc43186
Malthe Karbo <malthe@resights.dk>
2022-02-24 11:10:40 +0100

    Add foreign_key name to wrapper

--- sqlmodel/main.py
+++ sqlmodel/main.py
@@ -300,7 +300,7 @@ class SQLModelMetaclass(ModelMetaclass, DeclarativeMeta):
             # If it was passed by kwargs, ensure it's also set in config
             new_cls.__config__.table = config_table
             for k, v in new_cls.__fields__.items():
-                col = get_column_from_field(v)
+                col = get_column_from_field(v, cls=new_cls)
                 setattr(new_cls, k, col)
             # Set a config flag to tell FastAPI that this should be read with a field
             # in orm_mode instead of preemptively converting it to a dict.
@@ -425,7 +425,12 @@ def get_sqlalchemy_type(field: ModelField) -> Any:
     raise ValueError(f"The field {field.name} has no matching SQLAlchemy type")
 
 
-def get_column_from_field(field: ModelField) -> Column:  # type: ignore
+_TSQLModel = TypeVar("_TSQLModel", bound="SQLModel")
+
+
+def get_column_from_field(
+    field: ModelField, cls: Union[_TSQLModel, SQLModelMetaclass]
+) -> Column:  # type: ignore
     sa_column = getattr(field.field_info, "sa_column", Undefined)
     if isinstance(sa_column, Column):
         return sa_column
@@ -445,7 +450,12 @@ def get_column_from_field(field: ModelField) -> Column:  # type: ignore
     foreign_key = getattr(field.field_info, "foreign_key", None)
     unique = getattr(field.field_info, "unique", False)
     if foreign_key:
-        args.append(ForeignKey(foreign_key))
+        tablename = getattr(cls, "__tablename__", None)
+        if tablename is not None:
+            fk_name = f"{tablename}_{field.name}_fkey"
+            args.append(ForeignKey(foreign_key, name=fk_name))
+        else:
+            args.append(ForeignKey(foreign_key))
     kwargs = {
         "primary_key": primary_key,
         "nullable": nullable,
@@ -477,9 +487,6 @@ def _value_items_is_true(v: Any) -> bool:
     # Re-implement Pydantic's ValueItems.is_true() as it hasn't been released as of
     # the current latest, Pydantic 1.8.2
     return v is True or v is ...
-
-
-_TSQLModel = TypeVar("_TSQLModel", bound="SQLModel")
 
 
 class SQLModel(BaseModel, metaclass=SQLModelMetaclass, registry=default_registry):
